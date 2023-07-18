a3674ca734b003c0de60200ed7ad67dd5175c895
Diego França <diego.franca92@gmail.com>
2023-07-18 16:26:09 -0300

    feat: crud with serializer and rest framework

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Categoria
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+
 import json
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -46,3 +53,43 @@ class CategoriaView(View):
         }
 
         return JsonResponse(data)
+    
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+
+class CategoriaList(APIView):
+    def get(self, request):
+        categorias = Categoria.objects.all()
+        serializer = CategoriaSerializer(categorias, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategoriaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoriaDetail(APIView):
+    def get(self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        serializer = CategoriaSerializer(categoria)
+        return Response(serializer.data)
+   
+    def put(self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        serializer = CategoriaSerializer(categoria, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        categoria.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- livraria/urls.py
+++ livraria/urls.py
@@ -6,5 +6,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias', views.CategoriaView.as_view()),
-    path('categorias/<int:id>', views.CategoriaView.as_view())
+    path('categorias', views.CategoriaView.as_view()),
+    path('categorias/<int:id>', views.CategoriaView.as_view()),
+    path('categorias/apiview', views.CategoriaList.as_view()),
+    path('categorias/apiview/<int:id>', views.CategoriaDetail.as_view()),
 ]
