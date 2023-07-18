c25e3004aaf0781dcf89696f9d98c928e466a0e2
Diego França <diego.franca92@gmail.com>
2023-07-18 01:31:34 -0300

    modelagem das estruturas

---
+++ core/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from core.models import Autor, Categoria, Compra, Editora, ItensCompra, Livro
+
+admin.site.register(Autor)
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Livro)
+
+
+class ItensInline(admin.TabularInline):
+  model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+  inlines = (ItensInline,)
